b8322fbf0cff9d82efd2bee993b16ff1dcb36a8b
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-13 16:12:42 -0700

    capitalizeAndFilter for loop test passing

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,6 +1,19 @@
+// const capitalizeAndFilter = (arr) => {
+//   const uppercaseStrings = arr.map((str) => str.toUpperCase());
+//   const filteredArray = uppercaseStrings.filter((string) => string[0] !== 'F');
+//   return filteredArray;
+// };
+// export default capitalizeAndFilter;
+
 const capitalizeAndFilter = (arr) => {
-  const uppercaseStrings = arr.map((str) => str.toUpperCase());
-  const filteredArray = uppercaseStrings.filter((string) => string[0] !== 'F');
-  return filteredArray;
+  const filteredStrings = [];
+  let string;
+  for (const str of arr) {
+    string = str.toUpperCase();
+    if (string[0] !== 'F') {
+      filteredStrings.push(string);
+    }
+  }
+  return filteredStrings;
 };
 export default capitalizeAndFilter;
