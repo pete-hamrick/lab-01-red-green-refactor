e6fc93de0292a0e5fd7755aac92e82ed77b14317
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-13 16:13:37 -0700

    removed for loop and uncommented array methods

--- capitalize-and-filter.js
+++ capitalize-and-filter.js
@@ -1,19 +1,6 @@
-// const capitalizeAndFilter = (arr) => {
-//   const uppercaseStrings = arr.map((str) => str.toUpperCase());
-//   const filteredArray = uppercaseStrings.filter((string) => string[0] !== 'F');
-//   return filteredArray;
-// };
-// export default capitalizeAndFilter;
-
 const capitalizeAndFilter = (arr) => {
-  const filteredStrings = [];
-  let string;
-  for (const str of arr) {
-    string = str.toUpperCase();
-    if (string[0] !== 'F') {
-      filteredStrings.push(string);
-    }
-  }
-  return filteredStrings;
+  const uppercaseStrings = arr.map((str) => str.toUpperCase());
+  const filteredArray = uppercaseStrings.filter((string) => string[0] !== 'F');
+  return filteredArray;
 };
 export default capitalizeAndFilter;
