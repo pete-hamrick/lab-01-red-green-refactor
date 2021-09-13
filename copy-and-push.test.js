729ecc196f6824e6a8b3c6037d281b890597b7b4
Pete Hamrick <pete.hamrick@gmail.com>
2021-09-13 14:21:47 -0700

    numbers remains unchanged

--- copy-and-push.test.js
+++ copy-and-push.test.js
@@ -1,8 +1,11 @@
 import copyAndPush from './copy-and-push';
 
 describe('copyAndPush', () => {
+  const numbers = [1, 2, 3];
   it('should return a new array of 1, 2, 3, 4', () => {
-    const numbers = [1, 2, 3];
     expect(copyAndPush(numbers, 4)).toEqual([1, 2, 3, 4]);
+  });
+  it('test to make sure numbers is unchanged', () => {
+    expect(numbers).toEqual([1, 2, 3]);
   });
 });
--- get-name.test.js
+++ get-name.test.js
@@ -3,12 +3,12 @@ import getName from './get-name';
 describe('getName', () => {
   it('returns the name spot from the given object spot', () => {
     const spot = { name: 'spot', age: 5, weight: '20 lbs' };
-    
+
     expect(getName(spot)).toEqual('spot');
   });
   it('returns the name Aang from the given object character', () => {
     const character = { _id: '5cf5679a915ecad153ab68c9', name: 'Aang' };
-    
+
     expect(getName(character)).toEqual('Aang');
   });
 });
